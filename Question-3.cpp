e0eecca0770bf6b77c4278723c8debe4a5edea26
shazilhamzah <140880172+shazilhamzah@users.noreply.github.com>
2023-10-15 17:00:00 +0500

    Update Question-3.cpp

--- Question-3.cpp
+++ Question-3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
 
+double pi=3.14159;
+float area(int rad){
+    float a;
+    a = pi*pow(rad,2);
+    return a;
+}
+
+int main()
+{
+    int radius;
+    do
+    {
+        cout<<"Enter radius of a circle: "; cin>>radius;
+
+        if (radius<0)
+        {
+            cout<<"Enter a valid radius! (>0)"<<endl;
+        }
+        
+    } while (radius<0);
+    cout<<"Radius of circle is: "<<area(radius);
+    return 0;
+}
